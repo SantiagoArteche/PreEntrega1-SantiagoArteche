4f0b996893b936b0ed5b55de120f1600dabd01dc
santiago arteche <santiarteche@hotmail.com>
2023-05-16 10:07:29 -0300

    inicializando proyecto coder

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { Tarjeta } from "./Tarjeta";
+
+function App() {
+  return (<Tarjeta />
+  <h1>hola</h1>);
+}
+
+export default App;
---
+++ src/Tarjeta.jsx
@@ -0,0 +1,11 @@
+export function Tarjeta() {
+  let text = "como va";
+  let edad = 24;
+
+  return (
+    <div>
+      <h3>{text}</h3>
+      <h3>{edad}</h3>
+    </div>
+  );
+}
